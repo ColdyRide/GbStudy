b1748e72686d852c737a1ef4cb5b483f4306861c
ColdyRide <coldyride@gmail.com>
2019-11-08 17:56:02 +0700

    Code for exercise 2 was corrected, task description goes before code

--- Python/lesson_5/exercise_2.py
+++ Python/lesson_5/exercise_2.py
@@ -1,23 +1,10 @@
 # Lesson 4 Exercise 2
 # Представлен список чисел. Необходимо вывести элементы исходного списка, значения которых больше предыдущего элемента.
 
-
-def generator(user_list):
-    """
-    Функция для получения генератора из списка
-    :param user_list: list of numbers
-    :return: generator
-    """
-    try:
-        for elem in user_list.split():
-            yield float(elem)
-    except ValueError:
-        print('Please use numbers, not words!')
-
-
-raw_gen = generator(input('Please insert your list, separate by space - '))
-
-for i in raw_gen:
-    j = next(raw_gen)
-    if i < j:
-        print(j, end=' ')
+user_list = input('Please insert your list, separate by space - ').split()
+try:
+    result = [user_list[i] for i in range(1, len(user_list)) if float(user_list[i]) > float(user_list[i - 1])]
+    print('Result - ', end=' ')
+    [print(i, end=' ') for i in result]
+except ValueError:
+    print('Please use numbers, not words!')
