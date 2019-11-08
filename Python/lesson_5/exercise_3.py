e69e25f6d7de040a535937acc6c725089f5a7bc7
ColdyRide <coldyride@gmail.com>
2019-11-08 18:02:29 +0700

    Comment was corrected, task description goes before code

--- Python/lesson_5/exercise_3.py
+++ Python/lesson_5/exercise_3.py
@@ -1,4 +1,4 @@
-# Lesson 4 exercise 3
+# Lesson 4 Exercise 3
 # Для чисел в пределах от 20 до 240 найти числа, кратные 20 или 21. Необходимо решить задание в одну строку.
 
 [print(i, end=' ') for i in range(20, 241) if not i % 20 or not i % 21]
